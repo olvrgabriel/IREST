fbc67f8ffd825d192d62da74bf3fc4c8d95327f7
olvrgabriel <olvrgabriel07@gmail.com>
2026-04-01 17:32:57 -0300

    Migration para campos Email e Senha na tabela Funerarias
    Adiciona colunas Email e Senha (nullable) na tabela Funerarias
    para suportar login próprio das funerárias no sistema.

---
+++ IREST.API/IREST.API/Migrations/20260401203142_AddFunerariaCredentials.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IREST.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFunerariaCredentials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Funerarias",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Senha",
+                table: "Funerarias",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Funerarias");
+
+            migrationBuilder.DropColumn(
+                name: "Senha",
+                table: "Funerarias");
+        }
+    }
+}
